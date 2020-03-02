fddf33c0c9c73a932fad58e24e23a87c412d7b7a
Cenek Albl <cenek.albl@gmail.com>
2020-03-02 20:45:35 +0100

    minor changes

--- cython/rnppy.pyx
+++ cython/rnppy.pyx
@@ -10,26 +10,28 @@ cdef extern from "rnp.h":
 cdef extern from "rnp.h":
     int r6pDoubleLin(double * X, double * u, double r0, double * C, double * t, double *v, double * w, int direction)
 
-def r6pSingleLinpy(np.ndarray[double, ndim=2, mode="c"] X not None,
-            np.ndarray[double, ndim=2, mode="c"] u not None
+def r6pSingleLinpy(np.ndarray[double, ndim=2, mode="fortran"] X not None,
+            np.ndarray[double, ndim=2, mode="fortran"] u not None,
+            direction
             ):
     cdef np.ndarray[double, ndim=2, mode="fortran"] v = np.empty((3,64),order='fortran')
     cdef np.ndarray[double, ndim=2, mode="fortran"] C = np.empty((3,64),order='fortran')
     cdef np.ndarray[double, ndim=2, mode="fortran"] w = np.empty((3,64),order='fortran')
     cdef np.ndarray[double, ndim=2, mode="fortran"] t = np.empty((3,64),order='fortran')
 
-    nres = r6pSingleLin(<double*> np.PyArray_DATA(X), <double*> np.PyArray_DATA(u), 0,0,1, <double*> np.PyArray_DATA(v), <double*> np.PyArray_DATA(w), <double*> np.PyArray_DATA(C), <double*> np.PyArray_DATA(t))
+    nres = r6pSingleLin(<double*> np.PyArray_DATA(X), <double*> np.PyArray_DATA(u), direction,0,1, <double*> np.PyArray_DATA(v), <double*> np.PyArray_DATA(w), <double*> np.PyArray_DATA(C), <double*> np.PyArray_DATA(t))
 
     return v[:,:nres], C[:,:nres], w[:,:nres], t[:,:nres]
 
-def r6pDoubleLinpy(np.ndarray[double, ndim=2, mode="c"] X not None,
-            np.ndarray[double, ndim=2, mode="c"] u not None
+def r6pDoubleLinpy(np.ndarray[double, ndim=2, mode="fortran"] X not None,
+            np.ndarray[double, ndim=2, mode="fortran"] u not None,
+            direction
             ):
     cdef np.ndarray[double, ndim=2, mode="fortran"] v = np.empty((3,64),order='fortran')
     cdef np.ndarray[double, ndim=2, mode="fortran"] C = np.empty((3,64),order='fortran')
     cdef np.ndarray[double, ndim=2, mode="fortran"] w = np.empty((3,64),order='fortran')
     cdef np.ndarray[double, ndim=2, mode="fortran"] t = np.empty((3,64),order='fortran')
 
-    nres = r6pDoubleLin(<double*> np.PyArray_DATA(X), <double*> np.PyArray_DATA(u), 0, <double*> np.PyArray_DATA(C), <double*> np.PyArray_DATA(t), <double*> np.PyArray_DATA(v), <double*> np.PyArray_DATA(w), 0)
+    nres = r6pDoubleLin(<double*> np.PyArray_DATA(X), <double*> np.PyArray_DATA(u), direction, <double*> np.PyArray_DATA(C), <double*> np.PyArray_DATA(t), <double*> np.PyArray_DATA(v), <double*> np.PyArray_DATA(w), 0)
 
     return v[:,:nres], C[:,:nres], w[:,:nres], t[:,:nres]
